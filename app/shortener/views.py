a9a97b49286a2243114f11731713317d94ef7ad1
Jed Davis <JDavis2759@gmail.com>
2017-09-24 15:09:36 -0500

    Refactored to remove unnecessy functions

--- app/shortener/views.py
+++ app/shortener/views.py
@@ -7,36 +7,6 @@ from .. import db, wordlist
 from ..models import UrlLink
 
 
-def create_new_url_link(url_input):
-    """
-    Checks that the URL input is valid, creates a new URL link, storing the pair in the database and returning it.
-    Raises ValueError if the URL is invalid.
-    """
-    if not url(url_input):
-        raise ValueError
-    while True:
-        url_key = choice(wordlist.first_list) + '-' + choice(wordlist.second_list)
-        if not UrlLink.query.filter_by(url_key=url_key).first():
-            new_url_link = UrlLink(url_key, url_input)
-            db.session.add(new_url_link)
-            db.session.commit()
-            return url_key
-
-
-def get_url_from_url_key(url_key_input):
-    """
-    Looks up the URL key passed in and returns the corresponding URL.
-    Raises NoResultFound if it finds no URL link.
-    """
-    query = UrlLink.query.filter_by(url_key=url_key_input).first()
-    if query:
-        query.update_last_used()
-        db.session.commit()
-        return query.url
-    else:
-        raise exc.NoResultFound
-
-
 @shortener.route('/')
 def index():
     """
@@ -52,9 +22,16 @@ def get_shortened_url(url_input):
     Returns error code 400 if the URL is not valid.
     """
     try:
-        url_key = create_new_url_link(url_input)
-        return jsonify(original_url=url_input,
-                       short_url=request.url_root + url_key)
+        if not url(url_input):
+            raise ValueError
+        while True:
+            url_key = choice(wordlist.first_list) + '-' + choice(wordlist.second_list)
+            if not UrlLink.query.filter_by(url_key=url_key).first():
+                new_url_link = UrlLink(url_key, url_input)
+                db.session.add(new_url_link)
+                db.session.commit()
+                return jsonify(original_url=url_input,
+                               short_url=request.url_root + url_key)
     except ValueError:
         abort(400)
 
@@ -66,6 +43,12 @@ def redirect_to_url(url_key_input):
     Returns error code 400 if the URL key is not found in the database.
     """
     try:
-        return redirect(get_url_from_url_key(url_key_input))
+        query = UrlLink.query.filter_by(url_key=url_key_input).first()
+        if query:
+            query.update_last_used()
+            db.session.commit()
+            return redirect(query.url)
+        else:
+            raise exc.NoResultFound
     except exc.NoResultFound:
         abort(404)
